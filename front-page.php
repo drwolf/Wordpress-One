5791d1fd062ba7d1df32c0372d26fbd1e89a595a
drwolf <derrobertwolf@gmx.de>
2016-10-13 09:02:52 +0200

    Front Page Simulation in the current WP Theme 2-16

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head><scipt src="obj_fui.js"></script></head>
+<body>
+<p id="demo"></p>
+<script>
+var user = {
+    name: "tom",
+    say: function (words) { alert(words); }
+};
+
+document.getElementById("demo").innerHTML = user.say("honk");
+</script>
+
+
+</body>
+</html>
